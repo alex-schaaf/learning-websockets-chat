8585746b10ea82333b65065c8008949dcbdccf77
Alex <17277800+alex-schaaf@users.noreply.github.com>
2022-09-06 09:24:36 +0200

    format: prettier

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,26 @@
+const ws = require("ws");
 
-const ws = require('ws')
-
-const wss = new ws.WebSocketServer({ port: 8000 })
+const wss = new ws.WebSocketServer({ port: 8000 });
 
 const clients = new Set();
 
 const messages = [];
 
-wss.on('connection', (socket) => {
-  clients.add(socket)
-  console.log(`Client connected. n=${clients.size}`)
+wss.on("connection", (socket) => {
+  clients.add(socket);
+  console.log(`Client connected. n=${clients.size}`);
 
-  socket.on('message', (data) => {
-    const message = data.toString()
-    console.log('Message received', message)
+  socket.on("message", (data) => {
+    const message = data.toString();
+    console.log("Message received", message);
 
     for (let client of clients) {
-      client.send(message)
+      client.send(message);
     }
-  })
+  });
 
   socket.on("close", () => {
-    console.log("Client disconnected")
-    clients.delete(socket)
-  })
-})
-
+    console.log("Client disconnected");
+    clients.delete(socket);
+  });
+});
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,62 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [messages, setMessages] = useState<{message: string, timestamp: Date}[]>([])
-  const [message, setMessage] = useState<string>('')
-  const socket = useRef<WebSocket | null>(null)
-  
+  const [messages, setMessages] = useState<
+    { message: string; timestamp: Date }[]
+  >([]);
+  const [message, setMessage] = useState<string>("");
+  const socket = useRef<WebSocket | null>(null);
+
   useEffect(() => {
-    socket.current = new WebSocket("ws://localhost:8000/ws")
+    socket.current = new WebSocket("ws://localhost:8000/ws");
 
     socket.current.onmessage = (event) => {
-      setMessages(messages => [{message: event.data, timestamp: new Date()}, ...messages])
-    }
+      setMessages((messages) => [
+        { message: event.data, timestamp: new Date() },
+        ...messages,
+      ]);
+    };
 
-    return () => socket?.current?.close()
-  }, [])
-
-
+    return () => socket?.current?.close();
+  }, []);
 
   return (
     <div className="p-8 space-y-4">
-      <h1 className='font-bold text-4xl'>Chat</h1>
-      
+      <h1 className="font-bold text-4xl">Chat</h1>
+
       <div className="border h-64 p-2">
-        {messages.map(message => <div className='flex gap-2'>
-          <div className="text-sm text-gray-500">{message.timestamp.toISOString()}</div>
-          <div className="text-sm">{message.message}</div>
-        </div>)}
+        {messages.map((message) => (
+          <div className="flex gap-2">
+            <div className="text-sm text-gray-500">
+              {message.timestamp.toISOString()}
+            </div>
+            <div className="text-sm">{message.message}</div>
+          </div>
+        ))}
       </div>
       <div className="space-x-4">
-        <input className='border px-2 py-1' type="text" value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Enter message..." />
-        <button className='bg-gray-500 text-white px-2 py-1 hover:bg-gray-600 hover:cursor-pointer' onClick={() => {
-          if (!message) return
-          socket?.current?.send(message)
-          setMessage('')
-        }}>Send message</button>
+        <input
+          className="border px-2 py-1"
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Enter message..."
+        />
+        <button
+          className="bg-gray-500 text-white px-2 py-1 hover:bg-gray-600 hover:cursor-pointer"
+          onClick={() => {
+            if (!message) return;
+            socket?.current?.send(message);
+            setMessage("");
+          }}
+        >
+          Send message
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
